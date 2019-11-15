3292d5c1ae736e624ba47ae0e0cee55a7e320072
Yoctopuce <dev@yoctopuce.com>
2019-11-15 12:18:39 +0100

    "New build based on revison 38155"

--- constants.cs
+++ constants.cs
@@ -56,7 +56,7 @@ namespace Yocto_BridgeCalibration
   class constants
   {
 
-    public static string buildVersion = "37796";
+    public static string buildVersion = "38155";
     private static string _configfile = Path.Combine(Application.UserAppDataPath, "config.xml");
     private static bool _configfileOveridden = false;
     public static int MAXRAWDATAROWS = 2000;
